1685d4f093f446c31558bf6b73136b81733221f4
Rafael Siqueira <rafael.siqueira@cornershopapp.com>
2021-06-28 16:50:01 -0300

    update api/create

--- pages/api/create.js
+++ pages/api/create.js
@@ -2,16 +2,12 @@ import { db } from '../../firebase-client'
 
 export default function handler(req, res) {
   if (req.method === 'POST') {
-    createUser(req.body)
-    res.status(200).json({ success: true })
+    const { id, login, avatar_url } = req.body
+    db.collection("devs").add({ id, login, avatar_url })
+      .then(() => res.status(200).json({ success: true }))
+      .catch((err) => res.status(400).json({ success: false }))
   }
   else {
     res.status(404)
   }
-}
-
-const createUser = async ({ id, login, avatar_url }) => {
-  await db.collection("devs").add({
-    id, login, avatar_url
-  })
 }
